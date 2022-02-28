898723ef4ff33bdc800a466ea9885e0e7ac6a8a0
joongwon <winterdawnlight@snu.ac.kr>
2022-02-28 22:42:34 +0900

    add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import styles from "../styles/Home.module.scss";
 import TimeTableEditor from "../components/TimeTableEditor";
 import { TimeTable } from "../types/TimeTable";
 import { useState } from "react";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   const [tt, setTt] = useState<TimeTable>({
@@ -10,9 +11,19 @@ const Home: NextPage = () => {
     slots: [],
   });
   return (
-    <div className={styles.columnsContainer}>
-      <TimeTableEditor value={tt} onChange={setTt} timeBegin={9} timeEnd={21} />
-    </div>
+    <>
+      <Head>
+        <title>시간표 만들기</title>
+      </Head>
+      <div className={styles.columnsContainer}>
+        <TimeTableEditor
+          value={tt}
+          onChange={setTt}
+          timeBegin={9}
+          timeEnd={21}
+        />
+      </div>
+    </>
   );
 };
 
